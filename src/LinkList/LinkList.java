362c437b020e36dd9a15d67b5217518cb55ce15a
kenolion <veikeith@yahoo.com>
2017-08-05 16:28:05 +0800

    LINK LIST

--- src/LinkList/LinkList.java
+++ src/LinkList/LinkList.java
@@ -5,136 +5,252 @@
  */
 package LinkList;
 
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
  *
- * @author Sean
+ * @author Terry
  */
-public class LinkList<T> implements LinkListInterface<T> {
-
-    private Node start;
-    private Node end;
-
-
-    public LinkList() {
-
-        start = null;
-        end = null;
- 
+public class LinkList<T> implements ListIteratorInterface<T>,Serializable{
+    private Node firstNode;
+    private int entriesCounter;
+    
+    public LinkList(){
+        clearList();
     }
-
+    
+    
+    @Override
     public boolean add(T newEntry) {
-        Node newNode = new Node(newEntry);
-        if (start == null) {
-            start = newNode;
-            end = start;
-        } else {
+         Node newNode = new Node(newEntry);
 
-            end.next = newNode;
-            end = newNode;
-        }
-        return true;
+    if (isEmpty()) {
+      firstNode = newNode;
+    }
+    else {
+      Node lastNode = getNodeAt(entriesCounter);
+      lastNode.setNext(newNode);
     }
 
+    entriesCounter++;
+    return true;
+    }
+
+    @Override
     public boolean add(int newPosition, T newEntry) {
-        boolean isSuccessful = true;
-        Node newNode = new Node(newEntry);
+        boolean addSuccess = true;
+
+    if ((newPosition >= 1) && (newPosition <= entriesCounter + 1)) {
       
-        Node nodeAfter;
- 
-    if ((newPosition >= 1) && (newPosition <= size + 1)) {
+        Node newNode = new Node(newEntry);
 
       if (isEmpty() || (newPosition == 1)) {     // case 1: add to beginning of list
-        newNode.next = start;
-        start = newNode;
+        newNode.setNext(firstNode);
+        firstNode = newNode;
       } else {								                      // case 2: list is not empty and newPosition > 1
-        Node nodeBefore = getNodeAt(newPosition - 1);                           
-        nodeAfter = nodeBefore.next;
+        Node nodeBefore = getNodeAt(newPosition - 1);
+        Node nodeAfter = nodeBefore.next;
         newNode.setNext(nodeAfter);
         nodeBefore.setNext(newNode);
       }
 
-    } else {
-      isSuccessful = false;
-    }
-    // 
-        return isSuccessful;
+      entriesCounter++;
+    } 
+    
+    else {
+      addSuccess = false;
     }
 
+    return addSuccess;
+    }
+
+    @Override
     public T remove(int givenPosition) {
         T result = null;
 
-        if (givenPosition >= 1 && givenPosition <= size) {
-            if (givenPosition == 1) {
-                result = start.data;
-                start = start.next;
-            } else if (givenPosition == ) {
+    if ((givenPosition >= 1) && (givenPosition <= entriesCounter)) {
+      if (givenPosition == 1) {      // case 1: remove first entry
+        result = firstNode.getData();     // save entry to be removed 
+        firstNode = firstNode.getNext();
+      } else {                         // case 2: givenPosition > 1
+        Node nodeBefore = getNodeAt(givenPosition - 1);
+        Node nodeToRemove = nodeBefore.next;
+        Node nodeAfter = nodeToRemove.next;
+        nodeBefore.setNext(nodeAfter); // disconnect the node to be removed
+        result = nodeToRemove.getData();  // save entry to be removed
+      }
 
-            } else {
-                Node nodeBefore = start;
-                for (int i = 1; i < givenPosition - 1; ++i) {
-                    nodeBefore = nodeBefore.next;		// advance nodeBefore to its next node
-                }
-                result = nodeBefore.next.data;
-                nodeBefore.next = nodeBefore.next.next;
-            }
-
-        }
-        return result;
+      entriesCounter--;
     }
 
-    public void clear() {
-        start = null;
-        end = null;
+    return result;
     }
 
+    public void clearList() {
+    
+    firstNode = null;
+    entriesCounter = 0;    
+    
+    }
+
+    @Override
     public boolean replace(int givenPosition, T newEntry) {
-        return true;
+         boolean replaceSuccess = true;
+
+    if ((givenPosition >= 1) && (givenPosition <= entriesCounter)) {
+        Node desiredNode = getNodeAt(givenPosition);
+        desiredNode.setData(newEntry);
+    } 
+    
+    else {
+      replaceSuccess = false;
+     }
+
+    return replaceSuccess;    
+    
     }
 
+    @Override
     public T getEntry(int givenPosition) {
-        if (givenPosition == 1) {
-            return start.data;
-        } else if (givenPosition) {
-            return end.data;
-        } else {
-            Node tempNode = new Node();
-            tempNode = start;
-            for (int i = 2; i <= size; i++) {
-                tempNode = tempNode.next;
-                if (i == givenPosition) {
-                    return tempNode.data;
-                }
-            }
-        }
-        return null;
+        T result = null;
+
+    if ((givenPosition >= 1) && (givenPosition <= entriesCounter)) {
+      result = getNodeAt(givenPosition).getData();
+      
     }
 
+    return result;
+    }
+
+    @Override
     public boolean contains(T anEntry) {
-        return true;
+       boolean found = false;
+       Node currentNode = firstNode;
+
+    while (!found && (currentNode != null)) {
+      if (anEntry.equals(currentNode.getData())) {
+        found = true;
+      } else {
+        currentNode = currentNode.next;
+      }
     }
 
+    return found;
+    }
+
+    public int getNumberOfEntries() {
+        return entriesCounter;
+    }
+
+    @Override
     public boolean isEmpty() {
-        return start !=null;
+       boolean result;
+
+    if (entriesCounter == 0) {
+      result = true;
+    }
+    
+    else {
+      result = false;
     }
 
+    return result;
+    }
+
+    @Override
     public boolean isFull() {
-        return true;
+        return false;
     }
+    
+    public String toString() {
+    String outputStr = "";
+    Node currentNode = firstNode;
+    while (currentNode != null) {
+      outputStr += currentNode.getData() + "\n";;
+      currentNode = currentNode.next;
+    }
+    return outputStr;
+  }
 
-    private Node getNodeAt(int position) {
-    Node tempNode = start;
-        for (int i = 0; i < position; i++) {
-            tempNode = tempNode.next;
-        }
-        return tempNode;
+  private void displayChain(Node nodeOne) {
+    if (nodeOne != null) {
+      System.out.print(nodeOne.getData() + " ");
+      displayChain(nodeOne.next);
     }
+  }
+
+  /**
+   * Task: Returns a reference to the node at a given position. Precondition:
+   * List is not empty; 1 <= givenPosition <= numberOfEntries.
+   */
+  private Node getNodeAt(int givenPosition) {
+    Node currentNode = firstNode;
 
     // traverse the list to locate the desired node
-   
+    for (int counter = 1; counter < givenPosition; counter++) {
+      currentNode = currentNode.next;
+    }
 
-    private class Node {
-    private T data;
-    private Node next;
+    return currentNode;
+  }
+
+
+  public Iterator<T> getIterator() {
+    return new LinkedListIterator();
+  }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  private class LinkedListIterator implements Iterator<T> {
+     
+   
+   private Node currentNode;
+   
+   
+    public LinkedListIterator() {
+      currentNode = firstNode;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return currentNode != null;
+    }
+
+    @Override
+    public T next() {
+      if (hasNext()) {
+        
+       T returnData = (T)currentNode.data;
+       currentNode = currentNode.next;
+        
+       return returnData;
+      } 
+      
+      else {
+        throw new NoSuchElementException("Illegal call to next(); iterator is after end of list.");
+      }
+    }
+
+    @Override
+    public void remove() {
+        
+    }
+  }  
+  
+    private class Node implements Serializable{
+
+        private T data;
+        private Node next;
 
         Node() {
             data = null;
@@ -177,6 +293,6 @@ public class LinkList<T> implements LinkListInterface<T> {
         public void setNext(Node next) {
             this.next = next;
         }
-}
-
+    }
+  
 }
